7bdf8272e8f815c9a91559c9d6a4498819f89d0c
tom <alton.thomas.97@hotmail.co.uk>
2019-12-27 16:43:38 +0000

    created shaders

--- ThreeGPStart/Data/Shaders/fragment_shader.glsl
+++ ThreeGPStart/Data/Shaders/fragment_shader.glsl
@@ -1,8 +1,24 @@
 #version 330
 
+//uniform sampler2D sampler_tex;
+
 out vec4 fragment_colour;
+
+varying in vec3 v_normal;
+
+varying in vec2 TexCoord;
 
 void main(void)
 {
-	fragment_colour = vec4(1,1,1,1.0);
+	//vec3 N = normalize(v_normal);
+	//fragment_colour = vec4(N, 1.0f);
+
+	//fragment_colour = vec4(255.0f, 0.0f, 0.0f, 1.0f);
+
+	float n = v_normal.x;
+	fragment_colour = vec4(n, n, n, 1.0f);
+
+	//vec3 tex_colour = texture(sampler_tex, TexCoord).rgb;
+	//fragment_colour = vec4(tex_colour, 1.0f);
+
 }
--- ThreeGPStart/Data/Shaders/vertex_shader.glsl
+++ ThreeGPStart/Data/Shaders/vertex_shader.glsl
@@ -1,9 +1,21 @@
 #version 330
 
 // TODO
+uniform mat4 model_xform;
+uniform mat4 combined_xform;
+
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec3 vertex_normal;
+layout(location = 2) in vec2 vertex_uv;
+
+varying out vec3 v_normal;
+
+varying out vec2 TexCoord;
 
 void main(void)
 {	
-
+	v_normal = vertex_normal;
+	TexCoord = vertex_uv;
+	gl_Position = combined_xform * model_xform * vec4(vertex_position.x, vertex_position.y, vertex_position.z, 1.0);
 
 }
